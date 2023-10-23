c74bf63ab18d66b9f5d56942159055cba4151cc8
Art Karasev <as.karasyov@gmail.com>
2023-10-23 22:35:59 +1300

    extend robots

---
+++ src/Extensions/RobotsExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Goldfinch\Seo\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+
+class RobotsExtension extends DataExtension
+{
+    public function updateDisallowedUrls(&$urls)
+    {
+        $adminKey = array_search('/admin', $urls);
+        $devKey = array_search('/dev', $urls);
+
+        unset($urls[$adminKey]);
+        unset($urls[$devKey]);
+
+        // if (Robots::config()->disallow_unsearchable) {
+        //     foreach (SiteTree::get()->filter('ShowInSearch', false) as $page) {
+        //         $link = $page->Link();
+		// 		// Don't disallow home page, no RedirectorPage with RedirectionType External
+		// 		if ($link !== '/' && $page->RedirectionType != 'External') {
+        //             $urls[] = $link;
+        //         }
+        //     }
+        // }
+    }
+}
